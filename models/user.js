70373db1c5f0672d113651f38f6d97734e5bff1b
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2022-02-27 16:48:33 +0500

    feat(models): create user model

---
+++ models/user.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import crypto from 'crypto';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['admin', 'student'],
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
+
+userSchema.pre('save', function (next) {
+  this.password = bcrypt.hashSync(this.password, 10);
+});
+
+userSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString('hex');
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+  this.resetPasswordExpire = Date.now() + 10 * (60 * 1000);
+  return resetToken;
+};
+
+userSchema.statics.checkPassword = function (pass, hashedPass) {
+  return bcrypt.compareSync(pass, hashedPass);
+};
+
+export default mongoose.model('User', userSchema);
