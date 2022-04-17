728611a0a7ca50a97eec59fb86c60f3fabe8a444
Fatima Tuz Zehra <57491064+fatimacodes2001@users.noreply.github.com>
2022-04-17 13:34:12 +0500

    added tests

---
+++ __tests__/routes.t.js
@@ -0,0 +1,54 @@
+import request from 'supertest';
+import { app } from "../app.js"
+
+
+
+
+describe('Testing Poll Fetching Route', function () {
+
+  test('ID with Invalid format', async () => {
+
+    await request(app).get('/polls/62cd4fee7418ba78').then((res) => {
+      expect(res.statusCode).toBe(500);
+
+    });
+    
+  });
+  
+  test('Correct Admin ID', async () => {
+
+    await request(app).get('/polls/6238cd51ee7418ba7890b087').then((res) => {
+      expect(res.statusCode).toBe(200);
+
+    });
+
+  });
+
+  test('ID with a valid format that does not exist', async () => {
+
+    await request(app).get('/polls/6238cd4fee7418ba2890b983').then((res) => {
+      expect(res.statusCode).toBe(500);
+
+    });
+  
+    
+  });
+
+
+
+});
+
+
+
+describe('Testing Edit Route', function(){
+
+  test('responds to edit', async () => {
+
+    const res = await request(app).post('/polls/edit/625183c6a5922ce026818b96').send({poll_name:"Updated Poll Name"});
+    expect(res.statusCode).toBe(200);
+    
+  });
+
+
+
+})
--- controllers/polls.js
+++ controllers/polls.js
@@ -1,30 +1,42 @@
 import Poll from '../models/poll.js';
+import Admin from '../models/admin.js'
 import mongoose from 'mongoose';
 
 const getPolls = async (request, response) => {
-  console.log(request.params);
   var ObjectId = mongoose.Types.ObjectId;
-  let polls = await Poll.find({ admin: new ObjectId(request.params.adminId) });
-
+ 
   try {
+
+    let admin = await Admin.findById(request.params.adminId)
+    if(!admin){
+      throw error
+    }
+    let polls = await Poll.find({ admin: new ObjectId(request.params.adminId) });
     response.send(polls);
+
   } catch (error) {
-    console.log('Done');
+
+    console.log("Error")
     response.status(500).send(error);
+
   }
 };
 
 const editPoll = async (request, response) => {
+
   try {
     console.log(request.body);
     await Poll.findByIdAndUpdate(request.params.id, request.body);
     response.send('Done');
+
   } catch (error) {
     response.status(500).send(error);
   }
+
 };
 
 const createPoll = async (request, response) => {
+
   var ObjectId = mongoose.Types.ObjectId;
   await Poll.create({
     admin: new ObjectId(request.body.admin),
@@ -35,6 +47,7 @@ const createPoll = async (request, response) => {
   });
 
   response.send('Done');
+  
 };
 
 export { getPolls, createPoll, editPoll };
