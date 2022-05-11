04b3a923f46686bfbbaea0b6968b4d79d832b198
Fatima Tuz Zehra <57491064+fatimacodes2001@users.noreply.github.com>
2022-05-11 23:00:58 +0500

    Update poll-questions.js

---
+++ models/poll-questions.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const pollQSchema = new mongoose.Schema({
+  poll: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Poll',
+  },
+  text_based: {
+    type: mongoose.Schema.Types.Array
+  },
+  mcq: {
+    type: mongoose.Schema.Types.Array
+    
+  }
+  
+});
+
+const PollQues = mongoose.model('Poll-questions', pollQSchema);
+
+export default PollQues;
