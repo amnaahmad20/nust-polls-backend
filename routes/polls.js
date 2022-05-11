df7764973c1f2708688a198bea34976c7818bec1
Fatima Tuz Zehra <57491064+fatimacodes2001@users.noreply.github.com>
2022-05-11 23:03:10 +0500

    Update polls.js

--- routes/polls.js
+++ routes/polls.js
@@ -1,12 +1,16 @@
 import express from 'express';
-import { getPolls, createPoll, editPoll } from '../controllers/polls.js';
+import { getPolls, createPoll, editPoll, populatePoll } from '../controllers/polls.js';
 
 const pollRoutes = express.Router({ mergeParams: true });
 
-pollRoutes.get('/polls/:adminId', getPolls);
 
 pollRoutes.post('/polls/create', createPoll);
 
 pollRoutes.post('/polls/edit/:id', editPoll);
+
+pollRoutes.post('/polls/populate', populatePoll)
+
+pollRoutes.get('/polls/:adminId', getPolls);
+
 
 export default pollRoutes;
