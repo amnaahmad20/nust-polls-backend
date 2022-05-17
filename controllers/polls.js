de4ae228fcbd141d3d166cee09bbc2390f243428
mmaazrana <awaismaaz@gmail.com>
2022-05-17 23:00:51 +0500

    added poll deletion and question editing routes and controllers

--- controllers/polls.js
+++ controllers/polls.js
@@ -1,94 +1,108 @@
 import Poll from '../models/poll.js';
 import PollQues from '../models/poll-questions.js';
-import Admin from '../models/admin.js'
 import pollResponse from '../models/response.js';
 import mongoose from 'mongoose';
-import { request } from 'express';
 
 
 const getPolls = async (request, response) => {
-  var ObjectId = mongoose.Types.ObjectId;
-  let polls = await Poll.find({ admin: new ObjectId(request.params.adminId) });
+    var ObjectId = mongoose.Types.ObjectId;
+    let polls = await Poll.find({admin: new ObjectId(request.params.adminId)});
 
-  try {
-    response.send(polls);
-  } catch (error) {
-    console.log('Failure');
-    response.status(500).send(error);
-  }
+    try {
+        response.send(polls);
+    } catch (error) {
+        console.log('Failure');
+        response.status(500).send(error);
+    }
 
-  
+
 };
 
 const getDetails = async (request, response) => {
     var ObjectId = mongoose.Types.ObjectId;
-    let ques = await PollQues.find( {poll: new ObjectId(request.params.id)} );
-    let resp = await pollResponse.find( {poll: new ObjectId(request.params.id)} )
-    var details = { questions:ques, responses:resp }
+    let ques = await PollQues.find({poll: new ObjectId(request.params.id)});
+    let resp = await pollResponse.find({poll: new ObjectId(request.params.id)})
+    var details = {questions: ques, responses: resp}
     try {
-      response.send(details);
-    } 
-    catch (error) {
-      console.log('Failure');
-      response.status(500).send(error);
+        response.send(details);
+    } catch (error) {
+        console.log('Failure');
+        response.status(500).send(error);
     }
 
 
 }
 
 
-
 const editPoll = async (request, response) => {
+    try {
+        console.log(request.body);
+        console.log(request.params);
+        await Poll.findByIdAndUpdate(request.params.id, request.body);
+        response.send('Done');
+    } catch (error) {
+        response.status(500).send(error);
+    }
+};
 
+export const deletePoll = async (request, response) => {
+    try {
+        console.log(request.params);
+        await Poll.deleteOne({_id: request.params.id});
+        response.send('Deleted');
+    } catch (error) {
+        response.status(500).send(error);
+    }
+};
 
-  try {
-    console.log(request.body);
-    await Poll.findByIdAndUpdate(request.params.id, request.body);
-    response.send('Done');
-  } catch (error) {
-    response.status(500).send(error);
-  }
+export const editPollQues = async (request, response) => {
+
+    try {
+        console.log(request.body);
+        console.log(request.params);
+        await PollQues.findByIdAndUpdate(request.params.id, request.body);
+        response.send('Done');
+    } catch (error) {
+        response.status(500).send(error);
+    }
 
 
 };
-
-
-
 
 const createPoll = async (request, response) => {
-   let ObjectId = mongoose.Types.ObjectId;
-   
-   let newPoll = await Poll.create({
-      admin: new ObjectId(request.user._id),
-      poll_name: "Untitled",
-      description: "No Description",
-      deadline: new Date,
-      created_on: new Date,
-      published: false
+    let ObjectId = mongoose.Types.ObjectId;
+
+    let newPoll = await Poll.create({
+        admin: new ObjectId(request.user._id),
+        poll_name: "Untitled",
+        description: "No Description",
+        deadline: new Date,
+        created_on: new Date,
+        published: false
     });
 
-  response.send(newPoll);
+    response.send(newPoll);
 };
-
 
 
 const populatePoll = async (request, response) => {
-  try{
-
-    await PollQues.create(
-      request.body
-    )
-    
-    response.send("Done")
-
-  } catch(err){
-
-    console.log(err)
-    response.status(500).send(error);    
-
-  }
-
-
+    try {
+        let newQuestions = await PollQues.create(
+            {
+                mcq: [],
+                text_based: [
+                    {
+                        index: 0,
+                        statement: "Untitled Question"
+                    }
+                ]
+            }
+        )
+        response.send(newQuestions)
+    } catch (err) {
+        console.log(err)
+        response.status(500).send(error);
+    }
 }
 
-export { getPolls, createPoll, editPoll, populatePoll, getDetails };
+export {getPolls, createPoll, editPoll, populatePoll, getDetails};
--- routes/polls.js
+++ routes/polls.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getPolls, createPoll, editPoll, populatePoll, getDetails } from '../controllers/polls.js';
+import { getPolls, createPoll, editPoll, deletePoll, editPollQues, populatePoll, getDetails } from '../controllers/polls.js';
 import { protect, admin } from '../middlewares/auth.js';
 
 
@@ -9,6 +9,10 @@ const pollRoutes = express.Router({ mergeParams: true });
 pollRoutes.post('/polls/create', protect, admin, createPoll);
 
 pollRoutes.post('/polls/edit/:id', protect, admin, editPoll);
+
+pollRoutes.post('/polls/delete/:id', protect, admin, deletePoll);
+
+pollRoutes.post('/polls/edit-ques/:id', protect, admin, editPollQues);
 
 pollRoutes.post('/polls/populate', protect, admin, populatePoll)
 
